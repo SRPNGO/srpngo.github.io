2e26f7c51a9a101954d1de509802be627e03ebb7
Nsakrty <Nsakrty@163.com>
2025-06-22 12:48:26 +0800

    星球阁历史大修

--- script/floatPhotos.js
+++ script/floatPhotos.js
@@ -1,4 +1,4 @@
-let imgs = document.querySelectorAll("img");
+let imgs = [...document.querySelectorAll("img"),...document.querySelectorAll(".imageContainer")];
 for (let i = 1; i < imgs.length; i += 2) {
   imgs[i].style.float = 'left'
 }
